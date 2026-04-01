7c076399f7cb28b01767a6c97be1237fbf36c891
andrewdleach <type3dude@gmail.com>
2026-03-31 20:58:29 -0700

    feat: add crown favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,12 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxt/content',
   ],
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
 })
